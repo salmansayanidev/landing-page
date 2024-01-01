d806d2a162b2a8d4ce26c25a6f1429642d7b66a3
salmansayani <salmansayani822@gmail.com>
2024-01-01 23:54:32 +0500

    first

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 6,
+//     // spaceBetween: 120,
+//     pagination: {
+//       el: false,
+//       clickable: true,
+//     },
+//     // breakpoints: {
+//     //   640: {
+//     //     slidesPerView: 2,
+//     //   },
+//     //   768: {
+//     //     slidesPerView: 4,
+//     //   },
+//     // },
+//   });
